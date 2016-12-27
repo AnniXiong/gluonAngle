6f840a99acc4cfc2d3e556509229d10dbb8a81ed
Samuel Ross Meehan <smeehan12@gmail.com>
2016-12-27 11:33:48 +0100

    Anni should add comments

--- MC_assignment_2.py
+++ MC_assignment_2.py
@@ -1,30 +1,36 @@
+# Author : Anni Xiong
+#
+# Description :Put a descriptions here
+#
+
 import numpy as np
 import matplotlib.pyplot as plt
 import math
 
-
+# comment to describe these observables so that people can read the code more easily
+# "a" and "s" mean nothing to me
 a = 0.785
 s = 0.2
 L = []
 i = 0
 NOfEvent = 20000
 
-
+# comment to describe what you are doing with this loop
 while i < NOfEvent:
-	ns = np.random.uniform(0,10)
-	
-	x = np.random.uniform(0, (math.pi)/2)
-	f_g = (1/(0.399*s))*(math.exp( (-(x-a)**2)/(2*(s**2)) ))
-	
-	if ns < f_g:
-		L.append (x)
-	i = i+1
+    ns = np.random.uniform(0,10)
+
+    x = np.random.uniform(0, (math.pi)/2)
+    f_g = (1/(0.399*s))*(math.exp( (-(x-a)**2)/(2*(s**2)) ))
+
+    if ns < f_g:
+        L.append (x)
+    i = i+1
 
 
-
+# again, a small comment here to say that you are done with the important stuff and you
+# are now just plotting would be very helpful
 num_bins = 50
-n, bins, patches = plt.hist(L, num_bins,
-	                         normed = 1, facecolor='blue', alpha=0.5)
+n, bins, patches = plt.hist(L, num_bins, normed = 1, facecolor='blue', alpha=0.5)
 
 plt.xlabel('gluon emission angle')
 plt.ylabel('Probability')
--- README.md
+++ README.md
@@ -1,3 +1,13 @@
-# angle of gluon
+# Author
+
+# Description
 This program produces a histogarm of the angle that gluons flying off the interaction point which follows a Gaussian distribution 
 using accept and reject method.
+
+# Usage
+To run this program, just `git clone` the repository and then execute it with python
+```
+git clone https://github.com/XXXAAANNNi/gluonAngle.git
+cd gluonAngle
+python MC_assignment_2.py 
+```
